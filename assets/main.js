205bfde7f072674906a7872bf6e1ad6021228efd
Raselahmed971 <pitamchandra402@gmail.com>
2024-12-14 22:58:37 +0600

    mobile responsive

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,13 @@
 // navbar toggle
-const navToggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.links');
+const toggle_btn = document.querySelector('.toggle_btn');
+const mobile_nav_bar = document.querySelector('.mobile_nav_bar');
+const close_btn = document.querySelector('.close_btn');
+
+toggle_btn.addEventListener('click', () => {
+    mobile_nav_bar.classList.add('active');
+    mobile_nav_bar.style.transition = '1s';
+});
+close_btn.addEventListener('click', () => {
+    mobile_nav_bar.classList.remove('active');
+    mobile_nav_bar.style.transition = '1s';
+});
