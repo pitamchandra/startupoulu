23b5412fbe5441bb9dfa6bbfc97a2d2b65d3572a
Raselahmed971 <pitamchandra402@gmail.com>
2024-12-15 22:50:47 +0600

    event page done with full responsive

--- assets/main.js
+++ assets/main.js
@@ -11,3 +11,50 @@ close_btn.addEventListener('click', () => {
     mobile_nav_bar.classList.remove('active');
     mobile_nav_bar.style.transition = '1s';
 });
+
+// for carousel
+$('.event_wrapper').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          // centerMode: true,
+          // centerPadding: '30px',
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        
+        breakpoint: 767,
+        settings: {
+          centerMode: true,
+          centerPadding: '30px',
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // counter
+  $('.counter').counterUp({
+    delay: 20,
+    time: 2000
+});
---
+++ event.markdown
@@ -0,0 +1,5 @@
+---
+layout: event
+title: Event
+permalink: /event/
+---
